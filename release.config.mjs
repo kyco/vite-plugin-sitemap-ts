3eaf2ddc502e94f37ce97d9956ba67c537d62d10
Cornelius Weidmann <caweidmann.dev@gmail.com>
2026-03-19 15:00:38 +0100

    fix: Remove "docs" from commit analyzer

--- release.config.mjs
+++ release.config.mjs
@@ -13,7 +13,6 @@ const config = {
           { type: 'feat', release: 'minor' },
           { type: 'fix', release: 'patch' },
           { type: 'chore', release: 'patch' },
-          { type: 'docs', release: 'patch' },
           { type: 'style', release: 'patch' },
           { type: 'refactor', release: 'patch' },
           { type: 'perf', release: 'patch' },
